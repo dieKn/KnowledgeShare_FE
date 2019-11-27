a3e05d1c102dd2086828642188ac05d2104d348b
DieKn <d372kei@gmail.com>
2019-11-27 11:43:34 +0000

    test fix

--- webapp/src/router/index.ts
+++ webapp/src/router/index.ts
@@ -17,7 +17,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+   },
+   {
+    path: '/test',
+    name: 'test',
+    component: () => import('../views/Test.vue')
+   },
 ]
 
 const router = new VueRouter({
---
+++ webapp/src/views/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an test page</h1>
+    <Test msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import Test from '@/components/Test.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Test
+  }
+}
+</script>
